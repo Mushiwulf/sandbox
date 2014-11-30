cf37632a0e7e331bacf667c35e3172b494f8bc4d
Sean <mushiwulf@gmail.com>
2014-11-30 18:54:04 +0000

    Finished fullBattleshipGame. Works but could use some refinements. Namely, checking for mutliple hits in the same location and a way to reset the game.

--- javascript.js
+++ javascript.js
@@ -22,9 +22,9 @@ function fullBattleshipGame() {
         shipsSunk: 0,
         
     	ships: [
-	    	{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-	    	{ locations: ["24", "34", "44"], hits: ["", "", ""] },
-	    	{ locations: ["10", "11", "12"], hits: ["", "", ""] }
+	    	{ locations: ["0", "0", "0"], hits: ["", "", ""] },
+	    	{ locations: ["0", "0", "0"], hits: ["", "", ""] },
+	    	{ locations: ["0", "0", "0"], hits: ["", "", ""] }
     	],
         
         fire: function(guess) {
@@ -54,6 +54,52 @@ function fullBattleshipGame() {
                 }
             }
             return true;
+        },
+        
+        generateShipLocations: function() {
+            var locations;
+            for (var i = 0; i < this.numShips; i++) {
+                do {
+                    locations = this.generateShip();
+                } while (this.collision(locations));
+                this.ships[i].locations = locations;
+            }
+        },
+        
+        generateShip: function() {
+          var direction = Math.floor(Math.random() * 2);
+          var row, col;
+          
+          if (direction === 1) {
+              row = Math.floor(Math.random() * this.boardSize);
+              col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+          } else {
+              col = Math.floor(Math.random() * this.boardSize);
+              row = Math.floor(Math.random() * (this.boardSize - this.shipLength));              
+          }
+          
+          var newShipLocations = [];
+          for (var i = 0; i < this.shipLength; i++) {
+              if (direction === 1) {
+                  newShipLocations.push(row + "" + (col+i));
+              } else {
+                  newShipLocations.push((row + i) + "" + col);
+              }
+          }
+          return newShipLocations;
+         
+        },
+        
+        collision: function(locations) {
+            for (var i = 0; i < this.numShips; i++) {
+                var ship = model.ships[i];
+                for (var j = 0; j < locations.length; j++) {
+                    if (ship.locations.indexOf(locations[j]) >= 0) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     };
     
@@ -94,6 +140,7 @@ function fullBattleshipGame() {
         fireButton.onclick = handleFireButton;
         var guessInput = document.getElementById("guessInput");
         guessInput.onkeypress = handleKeyPress;
+        model.generateShipLocations();
     }
     function handleFireButton () {
         var guessInput = document.getElementById("guessInput");
