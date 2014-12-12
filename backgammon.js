5ef4bb3d475f419aa7b13ffe062f1d9b532927a7
Sean <mushiwulf@gmail.com>
2014-12-12 22:20:05 +0000

    Testing a move function. It needs to be broken down though

--- backgammon.js
+++ backgammon.js
@@ -7,16 +7,21 @@ function backgammonGame () {
         emptyCell: function(index) {
             var cell = document.getElementById(index);
             if (cell != "null") {
-                cell.setAttribute("class", "empty");              
+                cell.setAttribute("class", "null");              
             }
                
+        },
+        showDice: function(roll, die) {
+            var dieBin1 = document.getElementById("dieBin1");
+            dieBin1.innerHTML = "<p>" + die + "</p>";
         }
         
     };
     var model = {
-        playerTurn: "white",
+        playerTurn: "Black",
         startLocationsWhite: ["B0", "B1", "M0", "M1", "M2", "M3", "M4", "R0", "R1", "R2", "T0", "T1", "T2", "T3", "T4"],
         startLocationsBlack: ["Y0", "Y1", "N0", "N1", "N2", "N3", "N4", "I0", "I1", "I2", "G0", "G1", "G2", "G3", "G4"],
+        dice: [0,0],
         clearBoard: function() {
             for (var i = 0; i < model.startLocationsWhite.length; i++) {
                 var index = this.startLocationsWhite[i];
@@ -36,6 +41,11 @@ function backgammonGame () {
                 index = this.startLocationsBlack[i];
                 view.fillCell(index, "Black");
             }            
+        },
+        /* Right now I am going to make a function that does nothing to the model, just passes values from the controller to the view. I think inthe future I want to store things in an array in the model, so then this function will make more sense*/
+        updateChecker: function(oldLocation, newLocation, player) {
+            view.emptyCell(oldLocation);
+            view.fillCell(newLocation, player);
         }
     };
     var controller = {
@@ -48,31 +58,83 @@ function backgammonGame () {
             
         },
         advancePlayer: function() {
-            if (model.playerTurn === "white") {
-                model.playerTurn = "black"
+            if (model.playerTurn === "White") {
+                model.playerTurn = "Black"
             } else {
-                model.playerTurn = "white"
+                model.playerTurn = "White"
             }
         },
-        getActiveChecker: function(loction) {
+        getActiveChecker: function(location) {
             /* Take the index of the input and search the column for the last occupied slot*/
+
         },
         moveChecker: function(player, location, die) {
             var roll = this.die;
-            if (player === "white") {
-                var direction = 1;
+            var direction;
+            if (player === "White") {
+                direction = 1;
             } else {
                 direction = -1;
             }
             var move = roll*direction;
             
             var newLocation = String.fromCharCode(location.charCodeAt(0) + move);
+            model.updateChecker (location, newLocation, player);
             /* there is a lot of half baked stuff here that I ened to refine, but I wanted to get my general idea down */
+        },
+        rollDice: function() {
+            // loop through the dice array and grab two random numbers. Send to view.
+            var dice = model.dice;
+            for (var i = 0; i < dice.length; i++ ) {
+                dice[i] = Math.floor(Math.random()*6)+1;
+                view.showDice(dice[i], i);
+            }
+        },
+        // proof of concept function coming. Remove later
+        moveTest: function(column, player) {
+            player = model.playerTurn;
+            var newDirection;
+            if (player === "White") {
+                newDirection = 1;
+            } else {
+                newDirection = -1;
+            }
+            // newDirection not working properly (actually I think advancePlayer isnt working)
+            var newColumn = String.fromCharCode(column.charCodeAt(0) + newDirection);
+            var newRow = column.charAt(1);
+            var newIndex = newColumn + newRow;
+            view.emptyCell(column);
+            view.fillCell(newIndex, player);
+            controller.advancePlayer();
+            
         }
         
     };
     function init() {
         model.setupBoard();
+        var fireButton = document.getElementById("fireButton");
+        fireButton.onclick = handleFireButton;
+        var guessInput = document.getElementById("guessInput");
+        guessInput.onkeypress = handleKeyPress;        
+    }
+    
+        function handleKeyPress (e) {
+        var fireButton = document.getElementById("fireButton");
+        if (e.keyCode === 13) {
+            fireButton.click();
+            return false;
+        }
+    }
+    
+        function handleFireButton () {
+        var colInput = document.getElementById("guessInput");
+        var col = colInput.value;
+        controller.moveTest(col);
+        colInput.value = "";
+    }
+    
+    function handleRollDieButton() {
+        
     }
     init();
     
